29bfac999c9add422f04159d49d4dd11886df5c7
Matteo Miotto pro <matteo.miotto9@gmail.com>
2023-11-09 21:59:00 +0100

    fix sitemap issue

--- create_sitemap.R
+++ create_sitemap.R
@@ -11,14 +11,15 @@ rmd_files <- list.files(".", pattern = "\\.Rmd$", full.names = TRUE)
 sitemap <- xml2::xml_new_document()
 
 # Add the root element to the XML document
-root_node <- xml2::xml_add_child(sitemap, "urlset", xml_ns = "http://www.sitemaps.org/schemas/sitemap/0.9")
+root_node <- xml2::xml_add_child(sitemap, "urlset", 
+                                 xmlns = "http://www.sitemaps.org/schemas/sitemap/0.9")
 
 # Iterate over each R Markdown file and add it to the sitemap
 for (rmd_file in rmd_files) {
   url <- paste0(base_url, tools::file_path_sans_ext(basename(rmd_file)), "/")
   
   # Create a new 'url' element for each R Markdown file
-  url_node <- xml2::xml_add_child(root_node, "url", xml_ns = "http://www.sitemaps.org/schemas/sitemap/0.9")
+  url_node <- xml2::xml_add_child(root_node, "url")
   
   # Add 'loc' and 'changefreq' child elements to the 'url' element
   xml2::xml_add_child(url_node, "loc", url)
